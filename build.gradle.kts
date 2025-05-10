11375c81bd97c297c06827c5121485e47467d2ba
natanfudge <natan.lifsiz@gmail.com>
2025-05-10 14:13:15 +0300

    fix branch issues

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,7 @@ plugins {
     alias(libs.plugins.kotlin.serialization)
     id("org.graalvm.buildtools.native") version "0.10.6"
     id("com.google.osdetector") version "1.7.3"
-    id("org.jetbrains.compose.hot-reload") version "1.0.0-alpha10-118"
+    id("org.jetbrains.compose.hot-reload") version "1.0.0-alpha09"
 }
 
 repositories {
