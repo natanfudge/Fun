a7cd1273d896d2380af771c7f5b05cb6ff067813
natanfudge <natan.lifsiz@gmail.com>
2025-06-25 20:11:28 +0300

    Add wgpu4k-matrix link warning

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,21 +3,25 @@ pluginManagement {
         mavenCentral()
         google()
         gradlePluginPortal()
-        maven ("https://packages.jetbrains.team/maven/p/firework/dev")
+        maven("https://packages.jetbrains.team/maven/p/firework/dev")
     }
 }
 plugins {
-    id ("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
-    id ("de.fayard.refreshVersions") version "0.60.5"
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
+    id("de.fayard.refreshVersions") version "0.60.5"
 }
 rootProject.name = "Fun"
 
 val linkMatrix = true
 val matrixDir = file("../wgpu4k-matrix")
 if (linkMatrix) {
-    includeBuild(matrixDir) {
-        dependencySubstitution {
-            substitute(module("io.github.natanfudge:wgpu4k-matrix")).using(project(":"))
+    if (!matrixDir.exists()) {
+        println("Warn: linkMatrix is true but no ../wgpu4k-matrix folder is available, wgpu4k-matrix will be pulled from maven central instead.")
+    } else {
+        includeBuild(matrixDir) {
+            dependencySubstitution {
+                substitute(module("io.github.natanfudge:wgpu4k-matrix")).using(project(":"))
+            }
         }
     }
 }
